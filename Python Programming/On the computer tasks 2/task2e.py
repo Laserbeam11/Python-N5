1842fc9a9b4736f47b8c503ed012524ca07f4916
Sebastian Massey <S.Massey@albynschool.org>
2026-03-20 09:44:25 +0000

    3

--- Python Programming/On the computer tasks 2/task2e.py
+++ Python Programming/On the computer tasks 2/task2e.py
@@ -1,5 +1,5 @@
 #On The Computer: Basic Arithmetic
 #task 2e
 mass = int(input("what is the mass of the object in kg (numerical no notation nor units): "))
-Rs = (2 * (6.674*10 ** -11) * mass) / (299792458 ** 2)
+Rs = (2 * (6.674*10**-11) * mass) / (299792458 ** 2)
 print("the Schwarzschild radius of the object is:", Rs, "meters")
