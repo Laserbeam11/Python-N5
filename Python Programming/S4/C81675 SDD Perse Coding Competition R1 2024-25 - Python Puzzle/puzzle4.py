9d9d84647a0d335086c969ea56fba244ea072c2c
Sebastian Massey <S.Massey@albynschool.org>
2026-08-21 11:09:09 +0100

    done python puzzle

--- Python Programming/S4/C81675 SDD Perse Coding Competition R1 2024-25 - Python Puzzle/puzzle4.py
+++ Python Programming/S4/C81675 SDD Perse Coding Competition R1 2024-25 - Python Puzzle/puzzle4.py
@@ -1,16 +1,18 @@
 lengthOfSquarePacks = int(input("Enter the length of square packs being made: "))
 
+widthOfSquarePacks = lengthOfSquarePacks
+
 if lengthOfSquarePacks > 0 and lengthOfSquarePacks < 10:
     specialOffer = input("Is a special offer running? (y/n): ")
     if specialOffer == "y":
         additionalRows = int(input("Please enter the number of additional rows included for free: "))
+        widthOfSquarePacks = widthOfSquarePacks + additionalRows
     
-widthOfSquarePacks = widthOfSquarePacks + additionalRows
-
 totalNumberOfCans = lengthOfSquarePacks * widthOfSquarePacks
+#added---
+print()
+#--------
 
-widthOfSquarePacks = lengthOfSquarePacks
-
-print("The number of cans in the pack is: "+str(totalNumberOfCans))
+print("The number of cans in the pack is: "+str (totalNumberOfCans))
 
 
